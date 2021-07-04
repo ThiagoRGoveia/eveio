f69c8bdb520f06a109daacd17484a9320dbf7f06
Thiago <t.r.goveia@gmail.com>
2021-07-04 16:11:09 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <!-- <div class="container">
+          <div class="columns">
+            <div class="column">
+                <b-input v-model="data" />
+            </div>
+            <div class="column">
+                <b-input v-model="data" />
+            </div>
+          </div>
+      </div> -->
+      <section class="hero has-background-dark">
+        <div class="hero-body">
+            <div class = "container">
+                <div class = "columns">
+                    <div class = "column is-one-fifth">
+                        <h1>E-Veio</h1>
+                    </div>
+                    <div class = "column is-three-fifths is-0-desktop-only">
+                        <div class="field">
+                            <div class="control">
+                                <input class="input" type="search" placeholder="Exemplo: carne de soja">
+                            </div>
+                        </div>
+                    </div>
+                    <div class = "column is-one-fifth">
+                        <div class = "is-text is-pulled-right">
+                            <h1>Carrinho</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+      return {
+          data: ''
+      }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
