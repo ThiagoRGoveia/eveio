40a9266a84fcc74842368605030b141fa9e099a9
Matheus <matheusode@gmail.com>
2021-07-09 16:49:59 -0300

    Created Header and BottomPart files

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- Top part -->
+        <section class="hero has-background-dark">
+        <div class="hero-body">
+        <div class = "container">
+        <div class = "columns">
+            <div class = "column is-one-fifth">
+                <h1>E-Veio</h1>
+            </div>
+            <div class = "column is-three-fifths is-0-desktop-only">
+                <div class="field">
+                    <div class="control">
+                        <input class="input" type="search" placeholder="Exemplo: carne de soja">
+                    </div>
+                </div>
+            </div>
+            <div class = "column is-one-fifth">
+                <div class = "button is-pulled-right">
+                    <h1>Carrinho</h1>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        </section>
+        <!-- End of top part-->
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+
+<style>
+
+</style>
