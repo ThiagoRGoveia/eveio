ac6f5ef4b51012994c7f1d7044f9c75bf8a2f84f
Matheus <matheusode@gmail.com>
2021-07-13 18:18:15 -0300

    Added cart page and started the design. Also left bottom part for acessibility purposes

---
+++ src/components/CartHeader.vue
@@ -0,0 +1,30 @@
+<template>
+     <section class="hero is-danger">
+        <div class="hero-body">
+        <div class = "container">
+        <div class = "columns">
+            <div class = "column is-one-fifth">
+                <h1>E-Veio</h1>
+            </div>  
+            <div class = "column is-one-fifth">
+            </div>  
+            <div class = "column is-one-third is-centered">
+                <h1>Finalizar Compra</h1>
+            </div>  
+        </div>
+        </div>
+        </div>
+        </section>
+</template>
+
+<script>
+export default ({
+    name: 'CartHeader',
+    props:{
+    }
+})
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import Cart from './Cart.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '../node_modules/@mdi/font/css/materialdesignicons.css'
@@ -11,5 +12,6 @@ Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  //render: h => h(App),
+  render: h => h(Cart),
 }).$mount('#app')
