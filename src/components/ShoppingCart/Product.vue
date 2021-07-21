d1f0075ead3a4b72fc565c5f474181ac4a217e42
Thiago <t.r.goveia@gmail.com>
2021-07-21 20:35:42 -0300

    Adiciona logica para remover produto

--- src/components/ShoppingCart/Product.vue
+++ src/components/ShoppingCart/Product.vue
@@ -4,14 +4,21 @@
     <span>{{ product.ammount }}</span>
     <span>{{ product.unit }}</span>
     <span>{{ product.ammount * product.price}}</span>
-    <button>Remover</button>
+    <button @click="handleRemove">Remover</button>
   </span>
 </template>
 
 <script>
+import ShoppingList from '@/components/ShoppingList.js'
+
 export default {
   name: 'Product',
-  props: ['product']
+  props: ['product'],
+  methods: {
+    handleRemove () {
+      ShoppingList.remove(this.product)
+    }
+  }
 }
 </script>
 
