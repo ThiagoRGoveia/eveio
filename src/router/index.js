ba540625463b9990a4eeed856e75cae33c235968
Thiago <t.r.goveia@gmail.com>
2021-07-26 20:23:33 -0300

    ajusta configs de deploy

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,12 @@ import Router from 'vue-router';
 import ShoppingArea from '@/components/ShoppingArea/ShoppingArea.vue';
 import Checkout from '@/components/Checkout/Checkout.vue'
 import Payment from '@/components/Checkout/Payment.vue'
-
+import { publicPath } from '../../vue.config.js'
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
+  base: publicPath,
   routes: [
     {
       path: '/',
