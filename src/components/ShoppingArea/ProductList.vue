097b8c0c4ddd316dbea54f20438264d09c903cfc
Matheus <matheusode@gmail.com>
2021-07-28 21:33:15 -0300

    enhanced product list evem BEYOND

--- src/components/ShoppingArea/ProductList.vue
+++ src/components/ShoppingArea/ProductList.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
   <div class="is-flex is-flex-wrap-wrap i">
-    <div class="card-content column is-one-quarter" v-for="product in products" :key="product.id">
+    <div class="card-content column" v-for="product in products" :key="product.id">
       <product @add="handleAdd" :product="product"  />
     </div>
   </div>
