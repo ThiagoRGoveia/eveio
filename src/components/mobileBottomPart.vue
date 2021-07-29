e4eab8a2953d70a200639ff3e2393ae72694be4e
Matheus <matheusode@gmail.com>
2021-07-28 22:17:54 -0300

    eu sou brabo

---
+++ src/components/mobileBottomPart.vue
@@ -0,0 +1,123 @@
+<template>
+        <section class="sectionBottomPart">
+        <div class = "container bottomPart">
+        <div class = "columns is-multiline">
+            <div class = "column is-one-fifth is-flex is-justify-content-center is-align-items-center">
+                <router-link to="/">
+                  <img class="logos-app" src="@/assets/logo.svg" alt="home">
+                </router-link>
+                <br>
+            </div>
+            <div class = "column is-one-fifth">
+            </div>
+            <div class = "column is-one-fifth colunaEscolhida">
+                <br>
+                <br>
+                <a href="" class="links-uteis">Links Úteis</a>
+                <br>
+                <br>
+                <a href="" class="home">HOME</a>
+            </div>
+            <div class = "column is-one-fifth colunaEscolhida">
+                <a href="" class="fale-conosco"> Fale Conosco</a>
+                <br>
+                <br>
+                <a href="" class="faq">FAQ</a>
+            </div>
+            <div class = "column is-one-fifth">
+            </div>
+            <div class = "play-and-app-store">
+                <div class = "column is-one-fifth">
+                    <div class = "is-text">
+                        <img class="logos-app" src="@/assets/playstore.svg" alt="playstore">
+                    </div>
+                </div>
+                <div class = "column is-one-fifth">
+                    <div class = "is-text">
+                        <a href="">
+                        <img class="logos-app" src="@/assets/ios.svg" alt="appstore">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class = "column is-one-fifth informacoes">
+                <h1>Av. São Carlos 0001 - Centro</h1>
+                <h1>São Carlos - SP CEP: 13450-269</h1>
+                <h1>Email: contato@eveio.com.br</h1>
+            </div>
+            <div class = "column is-one-fifth is-0-desktop-only">
+
+            </div>
+            <div class = "column is-one-fifth">
+            </div>
+        </div>
+        </div>
+        </section>
+</template>
+
+<script>
+export default{
+    name: 'mobileBottomPart',
+    props: {
+    }
+}
+</script>
+
+<style>
+.sectionBottomPart {
+    background-color: #204B5F;
+    color: #fff;
+    padding: 0px;
+    margin-top: 5px;
+}
+.sectionBottomPart a {
+  color: white;
+}
+.colunaEscolhida {
+  text-align: center;
+}
+.logos-app {
+  height: 180px;
+}
+.links-uteis {
+  text-align: center;
+  color: white;
+  margin-bottom: 1rem;
+  font-size: 1.6rem;
+  font-weight: bold;
+  text-decoration: none;
+}
+.home {
+  color: white;
+  font-size: 1.6em;
+  font-weight: bold;
+  text-decoration: none;
+  margin-top: 10px;
+}
+.fale-conosco {
+  color: white;
+  font-size: 1.6rem;
+  font-weight: bold;
+  text-decoration: none;
+  margin-top: 10px;
+}
+.faq { 
+  color: white;
+  font-size: 1.6rem;
+  font-weight: bold;
+  text-decoration: none;
+  margin-top: 10px;
+}
+.play-and-app-store {
+    text-align: center;
+}
+.informacoes {
+    text-align: center;
+}
+/* meida for mobile */
+@media screen and (max-width: 768px) {
+  .logos-app {
+    height: 80px;
+  }
+}
+</style>
